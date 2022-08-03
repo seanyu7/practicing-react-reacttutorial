dcb838eed0f2d84b1721dbeb1fb6a9ee9ee36d80
seanyu7 <yuinzjumpuphand7@yahoo.co.jp>
2022-08-03 23:25:04 +0900

    ⚡️ make use of useReducer in my app like redux

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
 import "./App.css";
-import { useState, useContext, useRef } from "react";
+import { useState, useContext, useRef, useReducer } from "react";
 import YucodeInfoContext from ".";
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "increase":
+      return state + 1;
+    case "decrease":
+      return state - 1;
+    default:
+      return state;
+  }
+};
 
 function App() {
   const [count, setCount] = useState(0);
@@ -12,6 +23,8 @@ function App() {
   };
 
   const handleRef = () => {};
+
+  const [state, dispatch] = useReducer(reducer, 0);
 
   return (
     <div className="App">
@@ -25,6 +38,12 @@ function App() {
       <h1>useRef</h1>
       <input type="text" ref={ref} />
       <button onClick={handleRef}>useRef</button>
+
+      <hr />
+      <h1>useReducer</h1>
+      <p>count : {state}</p>
+      <button onClick = {()=> dispatch({type: "increase"})}>+</button>
+      <button onClick = {()=> dispatch({type: "decrease"})}>-</button>
     </div>
   );
 }
