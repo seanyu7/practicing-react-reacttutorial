cc984a0506f55b8a9782050c75aad8cf820a124b
seanyu7 <yuinzjumpuphand7@yahoo.co.jp>
2022-08-03 23:09:01 +0900

    ⚡️ make use of useRef in my app to get a reference

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import "./App.css";
-import { useState, useContext} from "react";
+import { useState, useContext, useRef } from "react";
 import YucodeInfoContext from ".";
 
 function App() {
   const [count, setCount] = useState(0);
   const yucodeInfo = useContext(YucodeInfoContext);
+  const ref = useRef();
 
   const handleClick = () => {
     setCount(count + 1);
   };
+
+  const handleRef = () => {};
 
   return (
     <div className="App">
@@ -17,6 +20,11 @@ function App() {
       <button onClick={handleClick}>+</button>
       <p>My name is {yucodeInfo.name}</p>
       <p>Yucode is {yucodeInfo.age} years old.</p>
+
+      <hr />
+      <h1>useRef</h1>
+      <input type="text" ref={ref} />
+      <button onClick={handleRef}>useRef</button>
     </div>
   );
 }
